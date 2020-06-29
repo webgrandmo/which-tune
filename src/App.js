e93515fa8a03c9d1b4c0424fbd6856312916a505
Andriy Trofimov <webgrandmo@gmail.com>
2020-06-29 17:11:43 +0300

    feature/tabs-component. Add Tabs component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container  from './Container';
 import Header from './header/Header';
+import Tabs from './tabs/Tab';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header logo="Which Tune App Logo" />
+      <Tabs />
       <Container />
     </div>
   );
